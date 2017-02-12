074d85c18ce86896d5feef7a7c6868cca5615e21
boboy <robertoguazon@gmail.com>
2017-02-12 10:51:01 +0800

    First commit

---
+++ Chariot of Wisdom/Assets/Scripts/GridCoords.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct GridCoords {
+	public int row, col;
+
+	public GridCoords(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+}
